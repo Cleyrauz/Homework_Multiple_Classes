094e6e252f5a641b8fdc9ae6640580cd13098eb6
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-22 19:21:29 +0100

    Tested a bear should have a name

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -3,7 +3,14 @@ require_relative("../bear")
 require_relative("../river")
 require_relative("../fish")
 
-
-
-
 class BearTest < MiniTest::Test
+
+  def setup
+    @bear = Bear.new("Pedroso", "Polar")
+  end
+
+  def test_bear_has_name
+    assert_equal("Pedroso", @bear.name)
+  end
+
+end 
